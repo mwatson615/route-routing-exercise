a668bc9d01ad76a6c143ef457654952c654bcd7e
Michael Watson <m.watson.tn@gmail.com>
2017-01-16 15:28:09 -0600

    exercise meets all specs

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("app.js")
+
+var app = angular.module('hwyApp', ['ngRoute'])
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/blueRidge.html', {
+			controller: 'MainCtrl',
+			templateUrl: 'partials/blueRidge.html'
+		})
+		.when('/natchez.html', {
+			controller: 'NatchezCtrl',
+			templateUrl: 'partials/natchez.html'
+		})
+})
+
+app.controller('MainCtrl', function($scope) {
+	$scope.highwayName = 'Blue Ridge Parkway';
+	$scope.highwayDescription = 'One of the prettiest drives in the US, winds through the Blue Ridge Mountains.';
+	// console.log('blueRidge')
+})
+
+app.controller('NatchezCtrl', function($scope) {
+	$scope.highwayName = 'Natchez Trace Parkway';
+	$scope.highwayDescription = "Travel from Franklin, TN to Natchez, MS on this gorgeous drive!"
+	// console.log('natchez')
+})
